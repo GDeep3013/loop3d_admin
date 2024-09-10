be2ebf9a39139abaa5785b47fe894aeb15a8aedd
Shiv Kumar Sharma <info.techieshiv@gmail.com>
2024-09-10 12:57:40 +0530

    working on  server js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors());
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName:process.env.MONGODB_DB_NAME });
+    await mongoose.connect(process.env.MONGODB_URI, {dbName:process.env.MONGODB_DB_NAME });
       console.log('MongoDB connection established');
   } catch (error) {
       console.error('Error connecting to MongoDB', error);
