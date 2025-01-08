aa51f558360c77de4db96c5f7f72f705474f97d7
Shiv Kumar Sharma <info.techieshiv@gmail.com>
2025-01-08 13:14:33 +0530

    remove console

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const multer = require("multer");
 const router = express.Router();
 const UserController = require('../app/controllers/UserController.js');
-const ProjectController = require('../app/controllers/ProjectController.js');
 const RoleController = require('../app/controllers/RoleController.js');
 
 const upload = multer({
@@ -27,11 +26,6 @@ router.get('/roles', RoleController.fetchRole);
 //Project controller Routes
 
 // router.post('/project-store', ProjectController.projectStore);
-router.post("/project-store", upload.array("files", 5), ProjectController.projectStore);
-router.get('/fetch-project', ProjectController.fetchProject);
-router.get('/show-project/:id', ProjectController.showProject);
-router.delete('/delete-project/:id', ProjectController.deleteProject);
-router.post("/update-project/:id", upload.array("files", 5), ProjectController.updateProject);
 router.post('/forget_password', UserController.forgetPassword);
 router.post('/reset-password', UserController.resetPassword);
 router.post('/create-password', UserController.createPassword);
