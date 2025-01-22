d50732adca71daa6d3a1e5662fe0ebcf406e9cd5
Shiv Kumar Sharma <info.techieshiv@gmail.com>
2025-01-22 11:20:59 +0530

    working on createVideo

--- emails/templates/sendSurveyCreationEmail.js
+++ emails/templates/sendSurveyCreationEmail.js
@@ -4,7 +4,7 @@ const sendSurveyCreationEmail = (recipientDetails) => {
                       <td>
                           <p>Welcome to LOOP3D! Our system is engineered to perpetually drive selfawareness and growth for leaders within your organization using a proprietery combination of artificial intelligence, doctoral level intervention and review, and personal customized AI development coaches. </p>
                           
-                          <p> In order to get the 360 feedback process started for [LOOP3D Lead], we need you to click the following link and select the top 3 most relevant competencies for [LOOP3D Lead],'s role.</p>
+                          <p> In order to get the 360 feedback process started for LOOP3D Lead, we need you to click the following link and select the top 3 most relevant competencies for LOOP3D Lead,'s role.</p>
 
                           <a style="display: inline-block; text-decoration:none;padding: 12px 25px;color: #fff; background-color: #7abcdb; border-radius: 5px;" href="${recipientDetails.url}">Create Survey</a>
                           <br />
